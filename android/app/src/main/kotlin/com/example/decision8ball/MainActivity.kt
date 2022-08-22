bfd51cc29f42f521ef08e2d017f669115fa6853c
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2022-08-22 13:08:04 +0530

    AppBar and color

---
+++ android/app/src/main/kotlin/com/example/decision8ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.decision8ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text("Ask Me Anything"),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Container(),
+      ),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
